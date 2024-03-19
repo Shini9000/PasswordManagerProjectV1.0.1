c0757d9164d4ac55d51b9c473f48983dc43c0b46
Aidan Christie <aidanchristie2232@gmail.com>
2024-03-19 20:40:30 +0000

    Update to V1.0.3

--- README.md
+++ README.md
@@ -1,36 +1,38 @@
-# PasswordManagerProject V1.0.2
-PasswordManagerProject V1.0.2  Remastered
+# PasswordManagerProject V1.0.3
+## Choose an option:
+#### 1. Generate Password
+Allows you to create a password 
+> 1. Uppercase # ABC
+> 2. Lowercase # abc
+> 3. Numbers # 123
+> 4. Mix # Ab3
+> 5. GenericPassword (Cap,Num and Sym # Ab3! 
+>> Additionally, you can choose to save password (Not implemented in this version)
+#### 2.View Passwords
+Not implemented yet, but will allow you to see 
+> Shows you all passwords as a list in the txt document.
+#### 3. View Accounts
+Not implemented yet, but will allow you to see 
+> Email, Name and Password (Keys (Not implemented) used for that account
+#### 4. Add Account
+Not yet implemented but will allow you to create an account using 
+> Email, Name and Password (Key (Not implemented)
+#### 5. Delete Account
+Not implemented yet, but will allow you to see 
+> Delete an account...
+#### 6. Help menu
+Opens the help menu
+#### 7. Exit
+Exits the program
 
-Choose an option: 
-1. Generate Password 
-    Allows you to create a password 
-2. View Passwords 
-    Not implemented as of yet, but will allow you to see 
-    - Shows you all passwords as a list in the txt document.
-3. View Accounts
-    Not implemented as of yet, but will allow you to see 
-    - Email, Name and Password (Keys (Not implemented) used for that account
-4. Add Account
-    Not yet implemented but will allow you to create an account using 
-    - Email, Name and Password (Key (Not implemented)
-5. Delete Account
-    Not implemented as of yet, but will allow you to see 
-    - Delete an account...
-6. Exit Application
-
------
-1. Uppercase "  # ABC
-2. Lowercase "  # abc
-3. Numbers "  # 123
-4. Mix "  # Ab3
-5. GenericPassword (Cap,Num and Sym"  # Ab3!
-6. Debug(Unsafe size) "  # Size of 200000 (Not implemented)
-
-Please report any bugs if you find any.
 -----
 
 # Imports
-As of version 1.0.2 
-import string
-import random
-import time
+As of version 1.0.3:
+- *string*
+- *random*
+- *time*
+
+-----
+GitHub repo: [PasswordManager](https://github.com/Shini9000/PasswordManagerProject)<br>
+Author: [Linktree](https://linktr.ee/shini9000)
--- main.py
+++ main.py
@@ -2,16 +2,112 @@ import string
 import random
 import time
 
-project_version = '1.0.2'
+project_version = '1.0.3'
+admin_signedin = False
 
 print(f"Welcome to the PasswordManager Application version: " + project_version +
       f"\nFor more information please go to the github @"
       f"\nhttps://github.com/Shini9000/PasswordManagerProject"
       f"\nAnd thank you for downloading and testing my application :)\n\n")
 
+""" safe keeping module
+def savepassword(result_string):
+    print(result_string)
+    if input(f'Do you wish to save this password?').capitalize() == 'Y':
+       print(f'Not implemented as of V1.0.3')#
+       admin_signedin = not False
+        admin_signon(admin_signedin)
+    else:
+        print(f'Returning to main menu!')
+
+
+def admin_signon(admin_signedin):
+    print(admin_signedin)
+    input('A')
+"""
+
+# Function to generate the password
+def generatepassword():
+    print(f"Generate a new password")
+    password_type_select = input(f"\n\nSelect an type: "
+                                 f"\n   1. Uppercase "  # ABC
+                                 f"\n   2. Lowercase "  # abc
+                                 f"\n   3. Numbers "  # 123
+                                 f"\n   4. Mix "  # Ab3
+                                 f"\n   5. GenericPassword (Cap,Num and Sym"  # Yz1!
+                                 f"\nEnter selection: ")
+    try:
+        selection = int(password_type_select)
+    except ValueError:
+        print(f"not a digit")
+    if selection not in (1, 2, 3, 4, 5):
+        print(f'Please select a valid option!')
+        generatepassword()
+    match selection:
+        case 1:
+            # Uppercase password generator
+            letters = string.ascii_uppercase
+            result_string = ''.join(
+                (random.choice(letters) for i in range(int(input("Select a password length: ")))))
+            result_string.startswith("\n")
+            print("\n" + result_string + "\n")
+            print(f'Returning to main menu!')
+        case 2:
+            # Lowercase password generator
+            letters = string.ascii_lowercase
+            result_string = ''.join(
+                (random.choice(letters) for i in range(int(input("Select a password length: ")))))
+            result_string.startswith("\n")
+            print("\n" + result_string + "\n")
+            print(f'Returning to main menu!')
+        case 3:
+            # Number password generator
+            numbers = string.digits
+            result_string = ''.join(
+                (random.choice(numbers) for i in range(int(input("Select a password length: ")))))
+            result_string.startswith("\n")
+            print("\n" + result_string + "\n")
+            print(f'Returning to main menu!')
+        case 4:
+            # Mix password generator
+            generic_letters = string.ascii_letters
+            numbers = string.digits
+            string_line = numbers + generic_letters
+            result_string = ''.join(
+                (random.choice(string_line) for i in range(int(input("Select a password length: ")))))
+            result_string.startswith("\n")
+            print("\n" + result_string + "\n")
+            print(f'Returning to main menu!')
+        case 5:
+            # Generic password generator
+            string_letters = string.ascii_letters
+            string_numbers = string.digits
+            string_symbols = string.punctuation
+            string_line = string_letters + string_symbols + string_numbers
+            result_string = ''.join(
+                (random.choice(string_line) for i in range(int(input("Select a password length: ")))))
+            result_string.startswith("\n")
+            print("\n" + result_string + "\n")
+            print(f'Returning to main menu!')
+    savepassword(result_string)
+
+
+def savepassword(result_string):
+    print(result_string)
+    if input(f'Do you wish to save this password?').capitalize() == 'Y':
+        print(f'Not implemented as of V1.0.3')
+        admin_signedin = not False
+        admin_signon(admin_signedin)
+    else:
+        print(f'Returning to main menu!')
+
+def admin_signon(admin_signedin):
+    print(admin_signedin)
+    input('A')
+
 # true loop
 while True:
-    time.sleep(3.5)
+    time.sleep(2.5)
     # Main menu loop
     user_input = input(f"Choose an option: "
                        f"\n     1. Generate Password "
@@ -19,83 +115,40 @@ while True:
                        f"\n     3. View Accounts "
                        f"\n     4. Add Account "
                        f"\n     5. Delete Account "
-                       f"\n     6. Exit Application "
+                       f"\n     6. Help menu"  # Help menu
+                       f"\n     7. Exit Application "
                        f"\nEnter Input: ")
     try:
         val = int(user_input)
     except ValueError:
         print(f"not a digit")
     # Check if input is valid
-    if val in (1, 2, 3, 4, 5, 6):
-        match val:
-            # Proceed to generate a password
-            case 1:
-                print(f"Generate a new password")
-                password_type_select = input(f"Select an type: "
-                                             f"\n1. Uppercase "  # ABC
-                                             f"\n2. Lowercase "  # abc
-                                             f"\n3. Numbers "  # 123
-                                             f"\n4. Mix "  # Ab3
-                                             f"\n5. GenericPassword (Cap,Num and Sym"  # Ab3!
-                                             f"\n6. Debug(Unsafe size) "  # Size of 200000
-                                             f"\n\nEnter selection: ")
-                try:
-                    selection = int(password_type_select)
-                except ValueError:
-                    print(f"not a digit")
-                match selection:
-                    case 1:
-                        letters = string.ascii_uppercase
-                        result_string = ''.join(
-                            (random.choice(letters) for i in range(int(input("Select a password length: ")))))
-                        result_string.startswith("\n")
-                        print("\n" + result_string + "\n")
-                        print(f'Returning to main menu!')
-                    case 2:
-                        letters = string.ascii_lowercase
-                        result_string = ''.join(
-                            (random.choice(letters) for i in range(int(input("Select a password length: ")))))
-                        result_string.startswith("\n")
-                        print("\n" + result_string + "\n")
-                        print(f'Returning to main menu!')
-                    case 3:
-                        numbers = string.digits
-                        result_string = ''.join(
-                            (random.choice(numbers) for i in range(int(input("Select a password length: ")))))
-                        result_string.startswith("\n")
-                        print("\n" + result_string + "\n")
-                        print(f'Returning to main menu!')
-                    case 4:
-                        generic_letters = string.ascii_letters
-                        numbers = string.digits
-                        string_line = numbers + generic_letters
-                        result_string = ''.join(
-                            (random.choice(string_line) for i in range(int(input("Select a password length: ")))))
-                        result_string.startswith("\n")
-                        print("\n" + result_string + "\n")
-                        print(f'Returning to main menu!')
-                    case 5:
-                        string_letters = string.ascii_letters
-                        string_numbers = string.digits
-                        string_symbols = string.punctuation
-                        string_line = string_letters + string_symbols + string_numbers
-                        result_string = ''.join(
-                            (random.choice(string_line) for i in range(int(input("Select a password length: ")))))
-                        result_string.startswith("\n")
-                        print("\n" + result_string + "\n")
-                        print(f'Returning to main menu!')
-            # Not implemented as of V1.0.1
-            case 2:
-                print(f'Test 2')
-            case 3:
-                print(f'Test 3')
-            case 4:
-                print(f'Test 4')
-            case 5:
-                print(f'Test 5')
-            # Exit the programme
-            case 6:
-                print(f'Test 6')
-                quit('Quit programme')
-    else:
-        print("Enter a valid number")
+    if val not in (1, 2, 3, 4, 5, 6, 7):
+        print(f'Please select a valid option!')
+        continue
+    match val:
+        # Proceed to generate a password
+        case 1:
+            generatepassword()
+        # Not implemented as of V1.0.1
+        case 2:
+            print(f'Password management not available in version: ' + project_version)
+        case 3:
+            print(f'Account management not available in version: ' + project_version)
+        case 4:
+            print(f'Test 4')
+        case 5:
+            print(f'Account management not available in version: ' + project_version)
+        case 6:
+            print(f'Help menu')
+            file = open("help.txt")
+            print(file.read())
+            input('Press enter to continue... ')
+            file.close()
+            print(f'Returning to main menu please wait.')
+            continue
+
+        # Exit the programme
+        case 7:
+            print(f'Test 7')
+            quit('Quit programme')
